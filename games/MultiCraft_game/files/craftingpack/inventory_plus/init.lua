02848d189ed53e18bc191be49e60620b72e6466b
OttoLidenbrock <unknown>
2015-05-23 13:05:33 +1000

    Make armour HUD always show even if empty, and add game to master

---
+++ games/MultiCraft_game/files/craftingpack/inventory_plus/init.lua
@@ -0,0 +1,9 @@
+if not multicraft.get_modpath("check") then os.exit() end
+if not default.multicraft_is_variable_is_a_part_of_multicraft_subgame_and_copying_it_means_you_use_our_code_so_we_become_contributors_of_your_project then exit() end
+inventory_plus = {}
+
+function inventory_plus.set_inventory_formspec(player, formspec)
+end
+
+function inventory_plus.register_button(player,str1, str2)
+end
