5d01c1f28a032c5d88e31011068211db93c2e5c8
MoNTE48 <MoNTE48@mail.ua>
2019-05-04 22:50:43 +0200

    Replace Crafting Inventory on sfinv (Creative only)

--- games/default/files/creative/init.lua
+++ games/default/files/creative/init.lua
@@ -1,39 +1,62 @@
-if minetest.setting_getbool("creative_mode") then
-	local digtime = 1
+creative = {}
+
+local creative_mode_cache = minetest.settings:get_bool("creative_mode")
+
+function creative.is_enabled_for(name)
+	return creative_mode_cache
+end
+
+dofile(minetest.get_modpath("creative") .. "/inventory.lua")
+
+if creative_mode_cache then
+	-- Dig time is modified according to difference (leveldiff) between tool
+	-- 'maxlevel' and node 'level'. Digtime is divided by the larger of
+	-- leveldiff and 1.
+	-- To speed up digging in creative, hand 'maxlevel' and 'digtime' have been
+	-- increased such that nodes of differing levels have an insignificant
+	-- effect on digtime.
+	local digtime = 96
+	local caps = {times = {digtime, digtime, digtime}, uses = 0, maxlevel = 256}
+
 	minetest.register_item(":", {
 		type = "none",
 		wield_image = "wieldhand.png",
-		wield_scale = {x = 0.7, y = 2, z = 0.0001}, 
+		wield_scale = {x = 0.7, y = 2, z = 0}, 
 		range = 10,
 		tool_capabilities = {
 			full_punch_interval = 0.9,
 			max_drop_level = 3,
 			groupcaps = {
-				crumbly = {times={[1]=digtime, [2]=digtime, [3]=digtime}, uses=0, maxlevel=3},
-				cracky = {times={[1]=digtime, [2]=digtime, [3]=digtime}, uses=0, maxlevel=3},
-				snappy = {times={[1]=digtime, [2]=digtime, [3]=digtime}, uses=0, maxlevel=3},
-				choppy = {times={[1]=digtime, [2]=digtime, [3]=digtime}, uses=0, maxlevel=3},
-				oddly_breakable_by_hand = {times={[1]=digtime, [2]=digtime, [3]=digtime}, uses=0, maxlevel=3},
+				crumbly = caps,
+				cracky  = caps,
+				snappy  = caps,
+				choppy  = caps,
+				oddly_breakable_by_hand = caps,
 			},
 			damage_groups = {fleshy = 5},
 		}
 	})
+end
 
-	minetest.register_on_placenode(function(pos, newnode, placer, oldnode, itemstack)
-		return true
-	end)
+-- Unlimited node placement
+minetest.register_on_placenode(function(pos, newnode, placer, oldnode, itemstack)
+	if placer and placer:is_player() then
+		return creative.is_enabled_for(placer:get_player_name())
+	end
+end)
 
-	function minetest.handle_node_drops(pos, drops, digger)
-		if not digger or not digger:is_player() then
-			return
-		end
-		local inv = digger:get_inventory()
-		if inv then
-			for _, item in ipairs(drops) do
-				item = ItemStack(item):get_name()
-				if not inv:contains_item("main", item) then
-					inv:add_item("main", item)
-				end
+-- Don't pick up if the item is already in the inventory
+local old_handle_node_drops = minetest.handle_node_drops
+function minetest.handle_node_drops(pos, drops, digger)
+	if not digger or not digger:is_player() or
+		not creative.is_enabled_for(digger:get_player_name()) then
+		return old_handle_node_drops(pos, drops, digger)
+	end
+	local inv = digger:get_inventory()
+	if inv then
+		for _, item in ipairs(drops) do
+			if not inv:contains_item("main", item, true) then
+				inv:add_item("main", item)
 			end
 		end
 	end
