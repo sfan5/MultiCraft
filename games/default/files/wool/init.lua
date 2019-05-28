459a54cb9b95a6a6da420db4c587c95e19a0a38a
MoNTE48 <MoNTE48@mail.ua>
2019-05-28 02:54:16 +0200

    3D hand! Fix wool sounds, HUD fix for Mobile, remove unused textures

--- games/default/files/wool/init.lua
+++ games/default/files/wool/init.lua
@@ -16,19 +16,6 @@ local dyes = {
 	{"dark_green", "Dark Green"},
 }
 
-function default.node_wool_defaults(table)
-	table = table or {}
-	table.footstep = table.footstep or
-		{name = "wool_coat_movement", gain = 1.0}
-	table.dug = table.dug or
-		{name = "wool_coat_movement", gain = 0.25}
-	table.place = table.place or
-		{name = "default_place_node", gain = 1.0}
-	return table
-end
-
-local wool_sound = default.node_wool_defaults()
-
 for i = 1, #dyes do
 	local name, desc = unpack(dyes[i])
 
@@ -38,7 +25,7 @@ for i = 1, #dyes do
 				is_ground_content = false,
 		groups = {snappy = 2, choppy = 2, oddly_breakable_by_hand = 3,
 			flammable = 3, wool = 1},
-		sounds = wool_sound,
+		sounds = default.node_wool_defaults(),
 	})
 
 	minetest.register_craft{
