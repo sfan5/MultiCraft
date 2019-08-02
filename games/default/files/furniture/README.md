100d2910a8794f4153398a4ff50ddf3c30da62d0
MoNTE48 <Maksym48@protonmail.com>
2019-08-02 22:49:44 +0200

    Furniture: add 3 new furniture, Some improvements

--- games/default/files/furniture/README.md
+++ games/default/files/furniture/README.md
@@ -7,6 +7,8 @@ This mod adds basic furniture (Chairs, Tables, Small Tables, Tiny Tables, Benche
 
 It was made by Thomas-S.
 
+Some node-boxes from https://forum.minetest.net/viewtopic.php?f=9&t=17421 (MIT).
+
 It is published under the MIT license.
 
 Code from "Get Comfortable [cozy]" (by everamzah; published under WTFPL) was used and modified.
--- games/default/files/furniture/init.lua
+++ games/default/files/furniture/init.lua
@@ -5,12 +5,12 @@ minetest.register_playerstep(function(dtime, playernames)
 		local player = minetest.get_player_by_name(name)
 		if player and player:is_player() then
 			if player_api.player_attached[name] and not player:get_attach() and
-					(player:get_player_control().up == true    or
-					 player:get_player_control().down == true  or
-					 player:get_player_control().left == true  or
+					(player:get_player_control().up    == true or
+					 player:get_player_control().down  == true or
+					 player:get_player_control().left  == true or
 					 player:get_player_control().right == true or
-					 player:get_player_control().jump == true) then
-				player:set_eye_offset({ x = 0, y = 0, z = 0 }, { x = 0, y = 0, z = 0 })
+					 player:get_player_control().jump  == true) then
+				player:set_eye_offset({x = 0, y = 0, z = 0}, {x = 0, y = 0, z = 0})
 				player:set_physics_override(1, 1, 1)
 				player_api.player_attached[player] = false
 				player_api.set_animation(player, "stand", 30)
@@ -22,13 +22,13 @@ end)
 ts_furniture.sit = function(name, pos)
 	local player = minetest.get_player_by_name(name)
 	if player_api.player_attached[name] then
-		player:set_eye_offset({ x = 0, y = 0, z = 0 }, { x = 0, y = 0, z = 0 })
+		player:set_eye_offset({x = 0, y = 0, z = 0}, {x = 0, y = 0, z = 0})
 		player:set_physics_override(1, 1, 1)
 		player_api.player_attached[name] = false
 		player_api.set_animation(player, "stand", 30)
 	else
 		player:moveto(pos)
-		player:set_eye_offset({ x = 0, y = -7, z = 2 }, { x = 0, y = 0, z = 0 })
+		player:set_eye_offset({x = 0, y = -7, z = 2}, {x = 0, y = 0, z = 0})
 		player:set_physics_override(0, 0, 0)
 		player_api.player_attached[name] = true
 		player_api.set_animation(player, "sit", 30)
@@ -37,7 +37,7 @@ end
 
 local furnitures = {
 	["chair"] = {
-		description = Sl("Chair"),
+		description = "Chair",
 		sitting = true,
 		nodebox = {
 			{ -0.3, -0.5,  0.2, -0.2,  0.5,  0.3 }, -- foot 1
@@ -45,7 +45,7 @@ local furnitures = {
 			{  0.2, -0.5, -0.3,  0.3, -0.1, -0.2 }, -- foot 3
 			{ -0.3, -0.5, -0.3, -0.2, -0.1, -0.2 }, -- foot 4
 			{ -0.3, -0.1, -0.3,  0.3,  0,    0.2 }, -- seating
-			{ -0.2,  0.1,  0.25, 0.2,  0.4,  0.26 } -- conector 1-2
+			{ -0.2,  0.1,  0.25, 0.2,  0.4,  0.26} -- conector 1-2
 		},
 		craft = function(recipe)
 			return {
@@ -56,7 +56,7 @@ local furnitures = {
 		end
 	},
 	["table"] = {
-		description = Sl("Table"),
+		description = "Table",
 		nodebox = {
 			{ -0.4, -0.5, -0.4, -0.3, 0.4, -0.3 }, -- foot 1
 			{  0.3, -0.5, -0.4,  0.4, 0.4, -0.3 }, -- foot 2
@@ -73,7 +73,7 @@ local furnitures = {
 		end
 	},
 	["small_table"] = {
-		description = Sl("Small Table"),
+		description = "Small Table",
 		nodebox = {
 			{ -0.4, -0.5, -0.4, -0.3, 0.1, -0.3 }, -- foot 1
 			{  0.3, -0.5, -0.4,  0.4, 0.1, -0.3 }, -- foot 2
@@ -89,7 +89,7 @@ local furnitures = {
 		end
 	},
 	["tiny_table"] = {
-		description = Sl("Tiny Table"),
+		description = "Tiny Table",
 		nodebox = {
 			{ -0.5, -0.1, -0.5,  0.5,  0,   0.5 }, -- table top
 			{ -0.4, -0.5, -0.5, -0.3, -0.1, 0.5 }, -- foot 1
@@ -103,7 +103,7 @@ local furnitures = {
 		end
 	},
 	["bench"] = {
-		description = Sl("Bench"),
+		description = "Bench",
 		sitting = true,
 		nodebox = {
 			{ -0.5, -0.1, 0,  0.5,  0,   0.5 }, -- seating
@@ -113,7 +113,59 @@ local furnitures = {
 		craft = function(recipe)
 			return {
 				{ recipe, recipe },
+				{ "group:stick", "group:stick" },
 				{ "group:stick", "group:stick" }
+			}
+		end
+	},
+	["bedsidetable"] = {
+		description = "Bedside Table",
+		nodebox = {
+			{ -0.5, -0.5, -0.5, -0.4, 0.5,  0.5 },
+			{  0.5, -0.5, -0.5,  0.4, 0.5,  0.5 },
+			{ -0.5,  0.4, -0.5,  0.5, 0.5,  0.5 },
+			{ -0.5,  0,   -0.5,  0.5, 0.1,  0.5 },
+			{ -0.5, -0.5,  0.5,  0.5, 0.5,  0.4 }
+		},
+		craft = function(recipe)
+			return {
+				{ recipe, recipe },
+				{ "group:stick", "group:stick" }
+			}
+		end
+	},
+	["endtable"] = {
+		description = "End Table",
+		nodebox = {
+			{ -0.5, -0.5, -0.5, -0.4,  0.5, -0.4 },
+			{ -0.5, -0.5,  0.5, -0.4,  0.5,  0.4 },
+			{  0.5, -0.5, -0.5,  0.4,  0.5, -0.4 },
+			{  0.5, -0.5,  0.5,  0.4,  0.5,  0.4 },
+			{  0.5,  0.4,  0.5, -0.5,  0.5, -0.5 },
+			{  0.5, -0.3,  0.5, -0.5, -0.2, -0.5 }
+		},
+		craft = function(recipe)
+			return {
+				{ recipe, "", recipe },
+				{ "group:stick", "", "group:stick" },
+				{ recipe, "", recipe },
+			}
+		end
+	},
+	["coffeetable"] = {
+		description = "Coffee Table",
+		nodebox = {
+			{ -0.5, -0.5, -0.5, -0.4,  0,   -0.4 },
+			{ -0.5, -0.5,  0.5, -0.4,  0,    0.4 },
+			{  0.5, -0.5, -0.5,  0.4,  0,   -0.4 },
+			{  0.5, -0.5,  0.5,  0.4,  0,    0.4 },
+			{  0.5,  0.1,  0.5, -0.5,  0,   -0.5 },
+			{  0.5, -0.3,  0.5, -0.5, -0.4, -0.5 }
+		},
+		craft = function(recipe)
+			return {
+				{ recipe, "", recipe },
+				{ "group:stick", "", "group:stick" },
 			}
 		end
 	},
@@ -124,7 +176,7 @@ local ignore_groups = {
 	["stone"] = true,
 }
 
-function ts_furniture.register_furniture(recipe, description, texture)
+function ts_furniture.register_furniture(recipe, description, tiles, fpairs)
 	local recipe_def = minetest.registered_items[recipe]
 	if not recipe_def then
 		return
@@ -138,10 +190,8 @@ function ts_furniture.register_furniture(recipe, description, texture)
 		end
 	end
 
-	for furniture, def in pairs(furnitures) do
-		local node_name = "ts_furniture:" .. recipe:gsub(":", "_") .. "_" .. furniture
-
-		def.on_rightclick = nil
+	for fpairs, def in pairs(fpairs) do
+		local node_name = "ts_furniture:" .. recipe:gsub(":", "_") .. "_" .. fpairs
 
 		if def.sitting then
 			def.on_rightclick = function(pos, node, player, itemstack, pointed_thing)
@@ -149,13 +199,16 @@ function ts_furniture.register_furniture(recipe, description, texture)
 			end
 		end
 
+		def.drawtype = def.drawtype or "nodebox"
+
 		minetest.register_node(":" .. node_name, {
-			description = description .. " " .. def.description,
-			drawtype = "nodebox",
+			description = description .. " " .. Sl(def.description),
+			drawtype = def.drawtype,
+			mesh = def.mesh,
 			paramtype = "light",
 			paramtype2 = "facedir",
 			sunlight_propagates = true,
-			tiles = { texture },
+			tiles = { tiles },
 			groups = groups,
 			node_box = {
 				type = "fixed",
@@ -171,8 +224,8 @@ function ts_furniture.register_furniture(recipe, description, texture)
 	end
 end
 
-ts_furniture.register_furniture("default:birch_wood", "Birch", "default_birch_wood.png")
-ts_furniture.register_furniture("default:pine_wood", "Pine", "default_pine_wood.png")
-ts_furniture.register_furniture("default:acacia_wood", "Acacia", "default_acacia_wood.png")
-ts_furniture.register_furniture("default:wood", "Wooden", "default_wood.png")
-ts_furniture.register_furniture("default:junglewood", "Jungle Wood", "default_junglewood.png")
+ts_furniture.register_furniture("default:birch_wood", "Birch", "default_birch_wood.png", furnitures)
+ts_furniture.register_furniture("default:pine_wood", "Pine", "default_pine_wood.png", furnitures)
+ts_furniture.register_furniture("default:acacia_wood", "Acacia", "default_acacia_wood.png", furnitures)
+ts_furniture.register_furniture("default:wood", "Wooden", "default_wood.png", furnitures)
+ts_furniture.register_furniture("default:junglewood", "Jungle Wood", "default_junglewood.png", furnitures)
