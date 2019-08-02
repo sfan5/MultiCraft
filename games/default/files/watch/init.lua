5661b3d8b91967c35bc6412a2ff9840924a741ae
MoNTE48 <Maksym48@protonmail.com>
2019-08-02 22:47:26 +0200

    Rewrite Watch Mod

--- games/default/files/watch/init.lua
+++ games/default/files/watch/init.lua
@@ -1,29 +1,7 @@
-function round(num)
-	return math.floor(num + 0.5)
-end
-
-minetest.register_playerstep(function(dtime, playernames)
-	local now = round((minetest.get_timeofday() * 24) % 12)
-	if now == 12 then now = 0 end
-	for i, name in ipairs(playernames) do
-		local player = minetest.get_player_by_name(name)
-		if player and player:is_player() then
-			local item = player:get_wielded_item()
-			if item and string.sub(item:get_name(), 0, 6) == "watch:" then
-				player:set_wielded_item("watch:"..now)
-			end
-			for i,stack in ipairs(player:get_inventory():get_list("main")) do
-				if i<9 and string.sub(stack:get_name(), 0, 6) == "watch:" then
-					player:get_inventory():remove_item("main", stack:get_name())
-					player:get_inventory():add_item("main", "watch:"..now)
-				end
-			end
-		end
-	end
-end, minetest.is_singleplayer()) -- Force step in singlplayer mode only
+local hour, inv
 
 local images = {
-	"watch_0.png",
+	"blank.png",
 	"watch_1.png",
 	"watch_2.png",
 	"watch_3.png",
@@ -34,19 +12,20 @@ local images = {
 	"watch_8.png",
 	"watch_9.png",
 	"watch_10.png",
-	"watch_11.png",
+	"watch_11.png"
 }
 
-local i
-for i, img in ipairs(images) do
-	local inv = 1
-	if i == 1 then
+for hour, img in ipairs(images) do
+	if hour == 1 then
 		inv = 0
+	else
+		inv = 1
 	end
-	minetest.register_tool("watch:"..(i-1), {
+
+	minetest.register_tool("watch:" .. (hour - 1), {
 		description = "Watch",
-		inventory_image = img,
-		groups = {not_in_creative_inventory=inv}
+		inventory_image = "watch_watch.png^" .. img,
+		groups = {not_in_creative_inventory = inv}
 	})
 end
 
@@ -58,3 +37,18 @@ minetest.register_craft({
 		{"", "default:gold_ingot", ""}
 	}
 })
+
+minetest.register_playerstep(function(dtime, playernames)
+	local now = math.floor((minetest.get_timeofday() * 24) % 12)
+	for hour, name in pairs(playernames) do
+		local player = minetest.get_player_by_name(name)
+		if player and player:is_player() then
+			for hour, stack in pairs(player:get_inventory():get_list("main")) do
+				if hour < 9 and string.sub(stack:get_name(), 0, 6) == "watch:" then
+					player:get_inventory():remove_item("main", stack:get_name())
+					player:get_inventory():add_item("main", "watch:" .. now)
+				end
+			end
+		end
+	end
+end)
